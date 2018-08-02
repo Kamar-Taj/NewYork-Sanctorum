909ff3a6e9a7978f3d7e2d5977019fa570be6856
Dr.Strange <41727521+Kamar-Taj@users.noreply.github.com>
2018-08-02 18:10:00 +0800

    Kprobe exmple
    Example of Kprobe

---
+++ Debugging/Kprobe/Kprobe.c
@@ -0,0 +1,42 @@
+//Kprobe.c - To keep things simple, I have created a small and easy-to-understand example. 
+//The target kernel function is ip_rcv(). The Kprobe example kernel module is as follows:
+
+#include<linux/module.h> 
+#include<linux/version.h> 
+#include<linux/kernel.h> 
+#include<linux/init.h> 
+#include<linux/kprobes.h> 
+ 
+static unsigned int counter = 0;
+int Pre_Handler(struct kprobe *p, struct pt_regs *regs){ 
+    printk("Pre_Handler: counter=%u\n",counter++);
+    return 0; 
+} 
+ 
+void Post_Handler(struct kprobe *p, struct pt_regs *regs, unsigned long flags){ 
+    printk("Post_Handler: counter=%u\n",counter++);
+} 
+ 
+static struct kprobe kp; 
+ 
+int myinit(void) 
+{ 
+    printk("module inserted\n "); 
+    kp.pre_handler = Pre_Handler; 
+    kp.post_handler = Post_Handler; 
+    kp.addr = (kprobe_opcode_t *)0xc071c9a9; 
+    register_kprobe(&kp); 
+    return 0; 
+} 
+ 
+void myexit(void) 
+{ 
+    unregister_kprobe(&kp); 
+    printk("module removed\n "); 
+} 
+ 
+module_init(myinit); 
+module_exit(myexit); 
+MODULE_AUTHOR("Dr.Strange"); 
+MODULE_DESCRIPTION("KPROBE MODULE"); 
+MODULE_LICENSE("GPL");
